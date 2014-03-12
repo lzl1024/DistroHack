1d485d44423b5c18c6e788ec25101140802a12c1
lzl1024 <zhuolinliu8@gmail.com>
2014-03-12 01:48:26 -0400

    link with bs and front page

--- distroHack/urls.py
+++ distroHack/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import patterns, url
 from distroHack import views
 
 urlpatterns = patterns('',
+
+
     # /polls/
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^polls/$', views.polls_index, name='polls_index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', views.polls_detail, name='polls_detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', views.polls_results, name='polls_results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', views.polls_vote, name='polls_vote'),
 )
