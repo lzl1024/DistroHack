4d72a5c679b5235b9f41f6253a16120c48b82135
lzl1024 <zhuolinliu8@gmail.com>
2014-03-16 01:59:43 -0400

    signIn/signup with ajax, small bugs still

--- distroHack/urls.py
+++ distroHack/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns('',
     url(r'^question/(?P<q_id>\d+)/$', views.question, name='question'),
     url(r'^runcode/$', views.runcode, name='runcode'),
     url(r'^updateq/$', views.update_question, name='updateQuestion'),
+    url(r'^signin/$', views.sign_in, name='sign_in'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^logout/$', views.logout, name='logout'),
+
 
     # /polls/
     url(r'^polls/$', views.polls_index, name='polls_index'),
--- distroHack/views.py
+++ distroHack/views.py
@@ -1,9 +1,11 @@
 import subprocess
 import random
 import os
+import django
 from threading import Lock
 import urllib
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
@@ -11,6 +13,7 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import sys
 import shutil
+import re
 
 from distroHack.models import Poll, Choice, Problem
 from dsProject.settings import OJ_PATH, PRO_PATH
@@ -186,4 +189,74 @@ def update_question(request):
     problem.testCode = read_fields(problem_dir, test_file_name)
     problem.save()
 
+    return redirect('distroHack.views.index')
+
+
+@csrf_exempt
+def sign_in(request):
+    name = request.POST["name"]
+    password = request.POST["password"]
+    validate = "failed"
+    result_msg = []
+
+    if not name or not password:
+        result_msg.append("UserName or Password should not be empty!")
+    else:
+        user = authenticate(username=name, password=password)
+        # authenticate success, login
+        if user is not None:
+            validate = "success"
+            login(request, user)
+        else:
+            result_msg.append("Name-Password pair is not registered!")
+
+    return render(request, 'hack/error.html', {'error': result_msg, 'msg': validate})
+
+
+@csrf_exempt
+def register(request):
+    # pick and initiate fields
+    email = request.POST["email"]
+    name = request.POST["userName"]
+    password = request.POST["password"]
+    com_password = request.POST["confirm"]
+    result_msg = []
+    validate = "failed"
+
+    # check valid information
+    if not email or not re.match('.+@.+', email):
+        result_msg.append("Email is invalid!")
+    elif User.objects.filter(email=email):
+        result_msg.append("This email has already been registered!")
+
+    if not password or not com_password:
+        result_msg.append("Password and Confirm Password should not be empty!")
+    else:
+        if password != com_password:
+            result_msg.append("Confirm Password is not the same as the Password!")
+        elif len(password) not in range(5, 21):
+            result_msg.append("The length of the password should between 5 and 20!")
+
+    if not name:
+        result_msg.append("UserName should not be empty!")
+    elif len(name) not in range(1, 201):
+        result_msg.append("The length of the UserName should between 1 and 200!")
+    elif User.objects.filter(username=name):
+        result_msg.append("This Username has already been registered!")
+
+    # create a new user and update database
+    if len(result_msg) == 0:
+        user = User.objects.create_user(username=name,
+                                    password=password,
+                                    email=email)
+        user.is_active = True
+        user.save()
+        validate = "success"
+        login(request, user)
+
+    return render(request, 'hack/error.html', {'error': result_msg, 'msg': validate})
+
+
+def logout(request):
+    django.contrib.auth.logout(request)
     return redirect('distroHack.views.index')
