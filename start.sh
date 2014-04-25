7ba70dc805375dbf5a26526c1e5f247183557afc
lzl1024 <zhuolinliu8@gmail.com>
2014-04-25 15:36:11 -0400

    start change

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 #go build superNode
 go build mainServer
 
-
-python manage.py syncdb
-
 if [ "$#" -eq 3  ]
 then
   $1/mainServer $2 $3 &
-  python manage.py runserver $3:8000
 else
   $1/mainServer $2 &
-  python manage.py runserver 8000
 fi
+
+
+python manage.py syncdb
+
+python manage.py runserver 8000
