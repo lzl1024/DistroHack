ed222dc5d2a9bd61c8ae9b1d09c2d6060bd4e0f2
lzl1024 <zhuolinliu8@gmail.com>
2014-04-25 15:14:15 -0400

    revise bootstrapper

---
+++ goServer/src/util/config.go
@@ -0,0 +1,11 @@
+package util
+
+import (
+
+)
+
+var SNConfigNames = map[string]string {
+		"bootstrapper.no-ip.biz":"bootstrapper.no-ip.biz",
+		"bootstrapper1.no-ip.biz":"bootstrapper1.no-ip.biz",
+		"bootstrapper2.no-ip.biz":"bootstrapper2.no-ip.biz",
+	}
